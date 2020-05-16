ab5339ef9144a45d6849d0c0c679f1e4a7532e22
travis Lawson <travislawclimb@gmail.com>
2020-05-16 02:30:34 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,6 +3,7 @@ def line(deli)
     puts "The line is currently empty"
   else  
     current_line = "The line is currently:"
-    deli.each.with_index(1) do |person, i|  
+    deli.each.with_index(1) do |person, i| 
+      current_line << " #{i}. #{person}"
   end  
 end
