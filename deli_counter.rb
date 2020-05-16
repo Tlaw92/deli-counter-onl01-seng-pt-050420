faccefe845537475c1abf66ee919bb81f7edfeff
travis Lawson <travislawclimb@gmail.com>
2020-05-16 02:29:52 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,6 +2,7 @@ def line(deli)
   if deli.empty
     puts "The line is currently empty"
   else  
-    
+    current_line = "The line is currently:"
+    deli.each.with_index(1) do |person, i|  
   end  
 end
