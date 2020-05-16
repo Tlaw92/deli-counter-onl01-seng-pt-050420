3706a2735749699fa0c681ec181029047860a001
travis Lawson <travislawclimb@gmail.com>
2020-05-16 02:38:29 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -18,11 +18,11 @@ def take_a_number(deli, name)
   puts "Welcome, #{name}. You are number #{deli.length} in line."
 end  
 
-def now_serving(deli)
-  if deli.empty?
-    puts "There is nobody waiting to be served!"
-  else 
-    puts "currently serving #{deli.first}."
-    deli.shift
-  end
-end  
+# def now_serving(deli)
+#   if deli.empty?
+#     puts "There is nobody waiting to be served!"
+#   else 
+#     puts "currently serving #{deli.first}."
+#     deli.shift
+#   end
+# end  
